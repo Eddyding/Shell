9b402524e88e685d693443d742bbdd5e451abe5c
Eddyding <920398694@qq.com>
2017-06-22 10:50:26 +0800

    01.sh

--- 01.sh
+++ 01.sh
@@ -3,9 +3,9 @@
 myfile="~/Desktop/file.list"
 
 if [ -f "$myfile" ]; then
-    echo  bye >  jjj
+	ls > ~/Desktop/file.list    
     exit 0
 else
     cd ..
-    ls > ~/Desktop/file.list
+    echo  bye >  jjj
 fi
